efb1ea85ef048adfc66ffe037994979eef3df2f1
MyronRubakha95 <myron11r@gmail.com>
2021-12-15 22:38:26 +0200

    https://github.com/MyronRubakha95/HW1.git

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,26 +10,17 @@ public class Main {
         int X = 5;
         int b = 7;
         int e = 2;
-
-
         //Завдання 1:  x+y-a/2*(x+w)
-
-        int result1 = x+y-a/2*(x+w);
+        int result1 = x + y - a / 2 * (x + w);
         System.out.println("result1 = " + result1);
-
         // Завдання 2 : a*(x+x)/y-a*3
-
-        int result2 = a*(x+x)/y-a*3;
+        int result2 = a * (x + x) / y - a * 3;
         System.out.println("result2 = " + result2);
-
-
-        // Завдання 3 : a+b/x-y
-
-        int result3 = a+b/x-y;
+        // Завдання 3 : a + b / x - y
+        int result3 = a + b / x - y;
         System.out.println("result3 = " + result3);
-
         // Завдання 4 : X-e/2%e+1
-        int result4 = X-e/2%e+1;
+        int result4 = X - e / 2 % e + 1;
         System.out.println("result4 = " + result4);
     }
 }
